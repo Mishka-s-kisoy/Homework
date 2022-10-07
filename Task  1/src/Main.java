cf5d75e37fce31bbf6fb07a12d5e71b51e3c4b22
Shurik-sozdatelsaytov <85281156+Shurik-sozdatelsaytov@users.noreply.github.com>
2022-10-08 02:10:18 +0300

    Add files via upload

---
+++ Task  1/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class NumberDigits {
+
+    int a;
+    int b;
+
+    public NumberDigits(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sumOfSeven() {
+        int res = 0;
+        for (int i = a; i <= b; ++i){
+            if (i % 7 == 0 && i > 9 && i < 100) {
+                res += i % 10 + i / 10 % 100;
+            }
+        }
+        return res;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        NumberDigits sum = new NumberDigits(in.nextInt(), in.nextInt());
+        System.out.println(sum.sumOfSeven());
+    }
+}
